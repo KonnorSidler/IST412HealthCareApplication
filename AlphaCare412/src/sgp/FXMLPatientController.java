c1b8c968d0e55bc2c2f1aadd8c38c9c1d63ddf0c
WoodruffA122 <woodruff.r.alexander@gmail.com>
2019-04-04 15:49:00 -0400

    Created new patient view with one column of reports and a back button. Contorller is PatientController

---
+++ AlphaCare412/src/sgp/FXMLPatientController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sgp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author alexwoodruff
+ */
+public class FXMLPatientController implements Initializable {
+
+    @FXML
+    private Button BackButtonAction;
+    @FXML
+    private TableView<?> Table;
+    @FXML
+    private TableColumn<?, ?> patientname;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) {
+    }
+    
+}
